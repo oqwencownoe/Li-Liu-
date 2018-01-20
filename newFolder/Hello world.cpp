119de220fea4925352f925a5a4cc277e381654dd
lijiatong <34698758+oqwencownoe@users.noreply.github.com>
2018-01-20 10:13:51 +0800

    Update Hello world.cpp

--- newFolder/Hello world.cpp
+++ newFolder/Hello world.cpp
@@ -7,3 +7,10 @@ int main()
   cout << "Hello world!";
   return 0;
 }
+
+//java version
+public class Test {
+  public static void main(Stirng[] args){
+    System.out.println("Hello world");
+  }
+}
